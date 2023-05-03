8276c53e00b7f43f7a0b2cb04803ccecd77ff0b3
Panagiotis Gkionis <panagiotis.gkionis@wowinc.com>
2023-05-03 22:27:04 +0100

    fix(build): fix_build

--- src/lib/server/db/models/user.model.ts
+++ src/lib/server/db/models/user.model.ts
@@ -1,6 +1,6 @@
 /* eslint-disable func-names */
 import bcrypt from 'bcrypt';
-import crypto from 'crypto';
+// import crypto from 'crypto';
 import type {Document, Model} from 'mongoose';
 import {model, models, Schema} from 'mongoose';
 
@@ -109,34 +109,35 @@ userSchema.methods.comparePassword = async function (
 };
 
 // Generates a password token to reset code
-userSchema.methods.generateResetPasswordToken = function (): string {
-	const user = this as IUser;
-	const resetToken = crypto.randomBytes(20).toString('hex');
-	const hashedToken = crypto
-		.createHash('sha256')
-		.update(resetToken)
-		.digest('hex');
-	user.resetPasswordToken = hashedToken;
-	user.resetPasswordExpire = new Date(Date.now() + 10 * 60 * 1000); // Token valid for 10 minutes
-	user.save();
-	return hashedToken;
+userSchema.methods.generateResetPasswordToken = function (): any {
+	// const user = this as IUser;
+	// const resetToken = crypto.randomBytes(20).toString('hex');
+	// const hashedToken = crypto
+	// 	.createHash('sha256')
+	// 	.update(resetToken)
+	// 	.digest('hex');
+	// user.resetPasswordToken = hashedToken;
+	// user.resetPasswordExpire = new Date(Date.now() + 10 * 60 * 1000); // Token valid for 10 minutes
+	// user.save();
+	// return hashedToken;
 };
 
 // Compare password method
 userSchema.methods.resetPassword = async function (password: string) {
-	if (passwordValidator(password) === false)
-		return {
-			passwordChanged: false,
-			message:
-				'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character (!@#$%^&*()_+-=[]{};:\'",./<>?)',
-		};
-	const user = this as IUser;
-	user.password = password;
-	// Allows to change password only once per token
-	user.resetPasswordToken = null;
-	user.resetPasswordExpire = null;
-	user.save();
-	return {passwordChanged: true, message: 'Password succesfully changed'};
+	// if (passwordValidator(password) === false)
+	// 	return {
+	// 		passwordChanged: false,
+	// 		message:
+	// 			'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character (!@#$%^&*()_+-=[]{};:\'",./<>?)',
+	// 	};
+	// const user = this as IUser;
+	// user.password = password;
+	// // Allows to change password only once per token
+	// user.resetPasswordToken = null;
+	// user.resetPasswordExpire = null;
+	// user.save();
+	// return {passwordChanged: true, message: 'Password succesfully changed'};
+	console.log(password);
 };
 
 const User: Model<IUser> = models.User || model<IUser>('User', userSchema);
