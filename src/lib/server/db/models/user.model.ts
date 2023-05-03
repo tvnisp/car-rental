4e03e140bbb7243b9d969e44c3ef88dacb76109c
Panagiotis Gkionis <panagiotis.gkionis@wowinc.com>
2023-05-03 22:42:55 +0100

    fix: add_next_latest

--- src/lib/server/db/models/user.model.ts
+++ src/lib/server/db/models/user.model.ts
@@ -1,6 +1,5 @@
 /* eslint-disable func-names */
-import bcrypt from 'bcrypt';
-import crypto from 'crypto';
+// import crypto from 'crypto';
 import type {Document, Model} from 'mongoose';
 import {model, models, Schema} from 'mongoose';
 
@@ -94,9 +93,10 @@ userSchema.pre<IUser>('save', async function hashPassword(next) {
 	if (!this.isModified('password')) {
 		return next();
 	}
-	const salt = await bcrypt.genSalt(10);
-	const hash = await bcrypt.hash(this.password, salt);
-	this.password = hash;
+	// const salt = await bcrypt.genSalt(10);
+	// const hash = await bcrypt.hash(this.password, salt);
+	// this.password = hash;
+	// return next();
 	return next();
 });
 
@@ -104,39 +104,42 @@ userSchema.pre<IUser>('save', async function hashPassword(next) {
 userSchema.methods.comparePassword = async function (
 	candidatePassword: string
 ): Promise<boolean> {
-	const isMatch = await bcrypt.compare(candidatePassword, this.password);
-	return isMatch;
+	// const isMatch = await bcrypt.compare(candidatePassword, this.password);
+	// return isMatch;
+	console.log(candidatePassword);
+	return true;
 };
 
 // Generates a password token to reset code
-userSchema.methods.generateResetPasswordToken = function (): string {
-	const user = this as IUser;
-	const resetToken = crypto.randomBytes(20).toString('hex');
-	const hashedToken = crypto
-		.createHash('sha256')
-		.update(resetToken)
-		.digest('hex');
-	user.resetPasswordToken = hashedToken;
-	user.resetPasswordExpire = new Date(Date.now() + 10 * 60 * 1000); // Token valid for 10 minutes
-	user.save();
-	return hashedToken;
+userSchema.methods.generateResetPasswordToken = function (): any {
+	// const user = this as IUser;
+	// const resetToken = crypto.randomBytes(20).toString('hex');
+	// const hashedToken = crypto
+	// 	.createHash('sha256')
+	// 	.update(resetToken)
+	// 	.digest('hex');
+	// user.resetPasswordToken = hashedToken;
+	// user.resetPasswordExpire = new Date(Date.now() + 10 * 60 * 1000); // Token valid for 10 minutes
+	// user.save();
+	// return hashedToken;
 };
 
 // Compare password method
 userSchema.methods.resetPassword = async function (password: string) {
-	if (passwordValidator(password) === false)
-		return {
-			passwordChanged: false,
-			message:
-				'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character (!@#$%^&*()_+-=[]{};:\'",./<>?)',
-		};
-	const user = this as IUser;
-	user.password = password;
-	// Allows to change password only once per token
-	user.resetPasswordToken = null;
-	user.resetPasswordExpire = null;
-	user.save();
-	return {passwordChanged: true, message: 'Password succesfully changed'};
+	// if (passwordValidator(password) === false)
+	// 	return {
+	// 		passwordChanged: false,
+	// 		message:
+	// 			'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character (!@#$%^&*()_+-=[]{};:\'",./<>?)',
+	// 	};
+	// const user = this as IUser;
+	// user.password = password;
+	// // Allows to change password only once per token
+	// user.resetPasswordToken = null;
+	// user.resetPasswordExpire = null;
+	// user.save();
+	// return {passwordChanged: true, message: 'Password succesfully changed'};
+	console.log(password);
 };
 
 const User: Model<IUser> = models.User || model<IUser>('User', userSchema);
